ce8124db8faf065f5c36d7a93618b54bb24edc0c
Armand Deshais <armand.deshais@gmail.com>
2019-10-27 21:56:22 +0100

    adminModel

--- public/src/webappli/models/AdminModel.java
+++ public/src/webappli/models/AdminModel.java
@@ -1,4 +1,114 @@
 package webappli.models;
 
+import java.util.Date;
+
 public class AdminModel {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String identifiant;
+    private String password;
+    private Date dateNaiss;
+    private String adresse;
+    private String mailPro;
+    private String mailPerso;
+    private String telPro;
+    private String telPerso;
+    private Boolean SupAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(String identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateNaiss() {
+        return dateNaiss;
+    }
+
+    public void setDateNaiss(Date dateNaiss) {
+        this.dateNaiss = dateNaiss;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getMailPro() {
+        return mailPro;
+    }
+
+    public void setMailPro(String mailPro) {
+        this.mailPro = mailPro;
+    }
+
+    public String getMailPerso() {
+        return mailPerso;
+    }
+
+    public void setMailPerso(String mailPerso) {
+        this.mailPerso = mailPerso;
+    }
+
+    public String getTelPro() {
+        return telPro;
+    }
+
+    public void setTelPro(String telPro) {
+        this.telPro = telPro;
+    }
+
+    public String getTelPerso() {
+        return telPerso;
+    }
+
+    public void setTelPerso(String telPerso) {
+        this.telPerso = telPerso;
+    }
+
+    public Boolean getSupAdmin() {
+        return SupAdmin;
+    }
+
+    public void setSupAdmin(Boolean supAdmin) {
+        SupAdmin = supAdmin;
+    }
 }
