25c778018705d3770f45673948868d063352a0c2
mathD92 <math.delp76@gmail.com>
2019-11-05 11:56:25 +0100

    category

--- public/src/webappli/models/Categories.java
+++ public/src/webappli/models/Categories.java
@@ -1,4 +1,27 @@
 package webappli.models;
 
 public class Categories extends BaseModelORM {
+
+    private Integer id;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    private String nom;
+
 }
